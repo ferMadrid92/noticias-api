b012e720a144d8f6399ce17af21ee9e8689ca2e2
Fernando <fer.madridb@gmail.com>
2023-09-14 17:12:28 -0500

    first commit

---
+++ src/hooks/useNoticias.jsx
@@ -0,0 +1,9 @@
+import { useContext, useState } from "react";
+import NoticiasContext from "../context/NoticiasProvider";
+
+const useNoticias = () => {
+
+    return useContext(NoticiasContext)
+}
+
+export default useNoticias
